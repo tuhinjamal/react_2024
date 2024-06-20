1bbdde87ab8f25026cb761accf9287bc2affdff9
tuhinjamal <tuhinjamal1@gmail.com>
2024-06-20 19:19:02 +0600

    todo context api done

--- src/pages/contextAPI/Context.jsx
+++ src/pages/contextAPI/Context.jsx
@@ -5,6 +5,7 @@ import Card from "./components/Card";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
 import UserContextProvider from "@/context/UserContextProvider.jsx";
+import TodoList from "./TodoList";
 function Context() {
   const [themeMode, setThemeMode] = React.useState("light");
 
@@ -24,24 +25,31 @@ function Context() {
   }, [themeMode]);
   return (
     <>
-      <div className="bg-gray-600 text-white text-3xl p-4">
-        <UserContextProvider>
-          <Login />
-          <Profile />
-        </UserContextProvider>
-        <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
-          <div className="flex flex-wrap min-h-screen items-center">
-            <div className="w-full">
-              <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                <ThemeBtn />
-              </div>
+      <div className="bg-gray-600 text-white text-3xl p-4 grid grid-cols-12">
+        <div className="col-span-4">
+          <UserContextProvider>
+            <Login />
+            <Profile />
+          </UserContextProvider>
+        </div>
+        <div className="col-span-4">
+          <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+            <div className="flex flex-wrap min-h-screen items-center">
+              <div className="w-full">
+                <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                  <ThemeBtn />
+                </div>
 
-              <div className="w-full max-w-sm mx-auto">
-                <Card />
+                <div className="w-full max-w-sm mx-auto">
+                  <Card />
+                </div>
               </div>
             </div>
-          </div>
-        </ThemeProvider>
+          </ThemeProvider>
+        </div>
+        <div className="col-span-4">
+          <TodoList />
+        </div>
       </div>
     </>
   );
